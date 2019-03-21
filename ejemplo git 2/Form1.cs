d433e1d8d16c96f951be9b1d57172b1ce6950e07
Daniel Herrer Saez <danhs@hotmail.es>
2019-03-21 10:34:03 +0100

    segunda version

--- ejemplo git 2/Form1.cs
+++ ejemplo git 2/Form1.cs
@@ -16,5 +16,10 @@ namespace ejemplo_git_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Holii :)");
+        }
     }
 }
